c3b56753ce4e0260fddb2e60793732c4a5e5c989
Nathan Silvester <nathan.silvester13@gmail.com>
2016-01-28 12:33:17 -0700

    Other changes

--- Repository/WebControls/NewGreetingCardMaker.aspx
+++ Repository/WebControls/NewGreetingCardMaker.aspx
@@ -11,7 +11,7 @@
 		  <div>
 			<div style="BORDER-RIGHT: thin ridge; PADDING-RIGHT: 20px; BORDER-TOP: thin ridge; PADDING-LEFT: 20px; FONT-SIZE: x-small; PADDING-BOTTOM: 20px; BORDER-LEFT: thin ridge; WIDTH: 293px; PADDING-TOP: 20px; BORDER-BOTTOM: thin ridge; FONT-FAMILY: Verdana; HEIGHT: 486px; BACKGROUND-COLOR: lightyellow">Choose 
 				a background color:<br />
-				<asp:dropdownlist ID="lstBackColor" runat="server" Height="22px" Width="194px">
+				<asp:dropdownlist ID="lstBackColor" runat="server" Height="22px" Width="194px" AutoPostBack="true" OnSelectedIndexChanged="ControlChanged">
                     <asp:ListItem Value="White"></asp:ListItem>
                     <asp:ListItem Value="Red"></asp:ListItem>
                     <asp:ListItem Value="Green"></asp:ListItem>
@@ -20,16 +20,16 @@
 				</asp:dropdownlist>
 				<br /><br />
 				Choose a font:<br />
-				<asp:dropdownlist ID="lstFontName" runat="server" Height="22px" Width="194px"></asp:dropdownlist>
+				<asp:dropdownlist ID="lstFontName" runat="server" Height="22px" Width="194px" AutoPostBack="true" OnSelectedIndexChanged="ControlChanged"></asp:dropdownlist>
 				<br /><br />
 				Specify a numeric font size:<br />
-				<asp:textbox ID="txtFontSize" runat="server"></asp:textbox>
+				<asp:textbox ID="txtFontSize" runat="server" AutoPostBack="true" OnTextChanged="ControlChanged"></asp:textbox>
 				<br /><br />
 				Choose a border style:<br />
-				<asp:radiobuttonlist ID="lstBorder" runat="server" Height="59px" Width="177px" Font-Size="X-Small"></asp:radiobuttonlist>
+				<asp:radiobuttonlist ID="lstBorder" runat="server" Height="59px" Width="177px" Font-Size="X-Small" AutoPostBack="true" OnSelectedIndexChanged="ControlChanged"></asp:radiobuttonlist>
 				<br /><br />
-				<asp:CheckBoxList ID="picList" runat="server">
-					<asp:ListItem Value="birthday.png">Birthday</asp:ListItem>
+				<asp:CheckBoxList ID="picList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ControlChanged">
+					<asp:ListItem Value="birthday.png" >Birthday</asp:ListItem>
 					<asp:ListItem Value="christmas.png">Christmas</asp:ListItem>
 					<asp:ListItem Value="get_well.gif">Get Well</asp:ListItem>
 					<asp:ListItem Value="graduation.jpg">Graduation</asp:ListItem>
@@ -37,7 +37,10 @@
 				</asp:CheckBoxList>
                 <br /><br />
 				Enter the greeting text below:<br />
-				<asp:textbox ID="txtGreeting" runat="server" Height="85px" Width="240px" TextMode="MultiLine"></asp:textbox>
+				<asp:textbox ID="txtGreeting" runat="server" Height="85px" Width="240px" TextMode="MultiLine" AutoPostBack="true" OnTextChanged="ControlChanged"></asp:textbox>
+				<br /><br />
+				Who is this from: <br />
+				<asp:TextBox ID="nameBox" runat="server" AutoPostBack="true" OnTextChanged="ControlChanged"></asp:TextBox>
 				<br /><br />
 				<asp:button ID="cmdUpdate" runat="server" Height="24px" Width="71px" Text="Update" onclick="cmdUpdate_Click"></asp:button>
 			</div>
@@ -45,7 +48,9 @@
 			Height="507px" Width="339px" HorizontalAlign="Center"><br />&nbsp; 
 			    <asp:Label ID="lblGreeting" runat="server" Height="150px" Width="256px"></asp:Label>
 			    <br /><br /><br />
-				<asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+				<asp:PlaceHolder ID="picHolder" runat="server"></asp:PlaceHolder>
+				<br />
+				<asp:Label ID="fromlabel" runat="server"></asp:Label>
 		    </asp:panel>
 	      </div>
 		</form>
--- Repository/WebControls/NewGreetingCardMaker.aspx.cs
+++ Repository/WebControls/NewGreetingCardMaker.aspx.cs
@@ -18,46 +18,46 @@ namespace GreetingCardMaker
 		{
 			if (this.IsPostBack == false)
 			{
-                // Set font options.
+				// Set font options.
 				lstFontName.Items.Add("Times New Roman");
 				lstFontName.Items.Add("Arial");
 				lstFontName.Items.Add("Verdana");
 				lstFontName.Items.Add("Tahoma");
 
-                // Set border style options by adding a series of
-                // ListItem objects.
-                ListItem item = new ListItem();
+				// Set border style options by adding a series of
+				// ListItem objects.
+				ListItem item = new ListItem();
 
-                // The item text indicates the name of the option.
-                item.Text = BorderStyle.None.ToString();
+				// The item text indicates the name of the option.
+				item.Text = BorderStyle.None.ToString();
 
-                // The item value records the corresponding integer
-                // from the enumeration. To obtain this value, you
-                // must cast the enumeration value to an integer,
-                // and then convert the number to a string so it
-                // can be placed in the HTML page.
-                item.Value = ((int)BorderStyle.None).ToString();
+				// The item value records the corresponding integer
+				// from the enumeration. To obtain this value, you
+				// must cast the enumeration value to an integer,
+				// and then convert the number to a string so it
+				// can be placed in the HTML page.
+				item.Value = ((int)BorderStyle.None).ToString();
 
-                // Add the item.
-                lstBorder.Items.Add(item);
+				// Add the item.
+				lstBorder.Items.Add(item);
 
-                // Now repeat the process for two other border styles.
-                item = new ListItem();
-                item.Text = BorderStyle.Double.ToString();
-                item.Value = ((int)BorderStyle.Double).ToString();
-                lstBorder.Items.Add(item);
+				// Now repeat the process for two other border styles.
+				item = new ListItem();
+				item.Text = BorderStyle.Double.ToString();
+				item.Value = ((int)BorderStyle.Double).ToString();
+				lstBorder.Items.Add(item);
 
-                item = new ListItem();
-                item.Text = BorderStyle.Solid.ToString();
-                item.Value = ((int)BorderStyle.Solid).ToString();
-                lstBorder.Items.Add(item);
-			
+				item = new ListItem();
+				item.Text = BorderStyle.Solid.ToString();
+				item.Value = ((int)BorderStyle.Solid).ToString();
+				lstBorder.Items.Add(item);
+
 				// Select the first border option.
 				lstBorder.SelectedIndex = 0;
-            }
+			}
 		}
 
-		protected void cmdUpdate_Click(object sender, System.EventArgs e)
+		private void UpdateCard()
 		{
 			// Update the color.
 			pnlCard.BackColor = Color.FromName(lstBackColor.SelectedItem.Text);
@@ -81,19 +81,34 @@ namespace GreetingCardMaker
 			// Update the border style.
 			pnlCard.BorderStyle = (BorderStyle)Int32.Parse(lstBorder.SelectedItem.Value);
 
-			foreach(ListItem li in picList.Items)
+			foreach (ListItem li in picList.Items)
 			{
 				//check is li is selected
-				if(li.Selected)
+				if (li.Selected)
 				{
 					//instatiate a new image control and add it to the placeholder
 					System.Web.UI.WebControls.Image i = new System.Web.UI.WebControls.Image();
 					i.ImageUrl = li.Value;
-					PlaceHolder1.Controls.Add(i);
+					picHolder.Controls.Add(i);
 				}
 			}
 			// Set the text
 			lblGreeting.Text = txtGreeting.Text;
+
+			if (nameBox.Text != "")
+			{
+				fromlabel.Text = "From: " + nameBox.Text;
+			}
+		}
+
+		protected void ControlChanged(object sender, EventArgs e)
+		{
+			UpdateCard();
+		}
+	
+		protected void cmdUpdate_Click(object sender, EventArgs e)
+		{
+			UpdateCard();
 		}
 	}
 }
